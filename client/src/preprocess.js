9d98e1f150773420effff340003023c047fc1dde
Jack Denson <jackdenson47@gmail.com>
2022-01-21 18:48:16 -0500

    Added preprocessors for new specifications

--- client/src/preprocess.js
+++ client/src/preprocess.js
@@ -1,3 +1,35 @@
+function getLastSunday() {
+    let lastSunday = new Date();
+    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay() % 7);
+    return lastSunday;
+}
+
+function duplLastWeek(totals) {
+    let thisWk = getLastSunday();
+    let lastWk = new Date(thisWk);
+    lastWk.setDate(lastWk.getDate() - 7)
+
+    let withDupl = []
+
+    for(let total of totals) {
+        withDupl.push({...total, "lastWk": total.Week == thisWk.toLocaleDateString()})
+        if(total.Week == lastWk.toLocaleDateString()) {
+            withDupl.push({...total, "lastWk": true})
+        }
+    }
+
+    return withDupl
+}
+
+let weekTotals = data => {
+    let totals = []
+    for(let date in data) {
+        const sum = data[date].reduce((a, b) => a + b.Cost, 0)
+        totals.push({"Week": date, "Total Spent": sum})
+    }
+    return totals
+}
+
 module.exports = {
     flatten: data => {
         let flat = []
@@ -8,9 +40,14 @@ module.exports = {
     },
 
     thisWeek: data => {
-        let lastSunday = new Date();
-        lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay() % 7);
+        let lastSunday = getLastSunday();
     
         return data[lastSunday.toLocaleDateString()]
+    },
+
+    weekTotals,
+
+    weekTotalsWithThis: data => {
+        return duplLastWeek(weekTotals(data))
     }
 }
