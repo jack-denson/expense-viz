0eb721c47e9a68b5831168ec09feb0b4f6bf335f
Jack Denson <jackdenson47@gmail.com>
2022-01-20 23:57:34 -0500

    Backend queries Notion & bins

--- api/index.js
+++ api/index.js
@@ -1,87 +1,28 @@
-const { Client } = require('@notionhq/client');
-const fs         = require('fs');
-const viz        = require("./visualizeStatic.js");
 const express    = require('express');
+const {runQuery} = require('./notion-data')
 
-
-// Parse out credentials and database ID
-const NOTION_KEY = fs.readFileSync("api/notion-token.txt", "UTF8");
-const notionPages = JSON.parse(fs.readFileSync("api/notion-pages.json", "UTF8"));
-const NOTION_DB = notionPages.expenseDB
-
-// Init Notion client
-const notion = new Client({auth: NOTION_KEY});
 const app = express();
-
-async function getData(databaseId, ftr){
-    const response = await notion.databases.query({
-      database_id: databaseId,
-      filter: ftr
-    });
-    return response;
-}
-
-function getThisWeek(date=(new Date()).toLocaleDateString()){
-    let lastSunday = new Date(date);
-    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay() % 7);
-
-    let nextSunday = new Date(date);
-    nextSunday.setDate(nextSunday.getDate() + (7 - nextSunday.getDay()) % 7);
-
-    return [lastSunday, nextSunday]
-}
-
-function formatResult(result){
-
-    let results = result.results
-    results = results.map(x => x.properties)
-    results = results.map(function(x){
-        res = {}
-        res["created_time"] = x.Date.created_time
-        res["Category"] = x.Category.multi_select[0].name//.map(x => x.name)[0]
-        res["Cost"] = x.Cost.number
-        res["Name"] = x.Name.title.map(x => x.plain_text)[0]
-        return res
-    })
-    return results
-}
-
-let ss = getThisWeek()
-lsun = ss[0].toISOString()
-nsun = ss[1].toISOString()
-
-
-async function runQuery(){
-    queryFilter = 
-    {"and": [{
-                property: "Date",
-                date: {
-                    on_or_after: lsun,
-                }
-            },
-            {
-                property: "Date",
-                date: {
-                    before: nsun,
-                }
-            }]
-    }
-    
-    response = await getData(NOTION_DB, queryFilter)
-    return formatResult(response)
-}
-
-async function runViz(){
-    data = await runQuery()
-    viz.drawVisualizations(data)
-}
-
 
 app.get('/', (req, res) => {
     res.json({
         abc: 'defg'
     });
 });
+
+app.get('/specs', (req, res) => {
+  const specs = fs.readdirSync("viz-specs")
+  let specList = []
+  for(let spec of specs){
+      specList.append(JSON.parse(fs.readFileSync("viz-specs/" + spec + ".json", "UTF8")))
+  }
+
+  res.json(specList)
+})
+
+app.get('/data', async (req, res) => {
+  const data = await runQuery()
+  res.json(data)
+})
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
---
+++ api/notion-data.js
@@ -0,0 +1,65 @@
+const { Client } = require('@notionhq/client');
+const fs         = require('fs');
+
+// Parse out credentials and database ID
+const NOTION_KEY = fs.readFileSync("./notion-token.txt", "UTF8");
+const notionPages = JSON.parse(fs.readFileSync("./notion-pages.json", "UTF8"));
+const NOTION_DB = notionPages.expenseDB
+
+// Init Notion client
+const notion = new Client({auth: NOTION_KEY});
+
+async function getData(databaseId, ftr){
+    const response = await notion.databases.query({
+      database_id: databaseId
+    });
+    return response;
+}
+
+function getWeek(date){
+    let lastSunday = new Date(date);
+    lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay() % 7);
+
+    return lastSunday.toLocaleDateString()
+}
+
+function formatResults(result){
+
+    let results = result.results
+    results = results.map(x => x.properties)
+    results = results.map(function(x){
+        res = {}
+        res["created_time"] = x.Date.created_time
+        res["Category"] = x.Category.multi_select[0].name
+        res["Cost"] = x.Cost.number
+        res["Name"] = x.Name.title.map(x => x.plain_text)[0]
+        return res
+    })
+    return results
+}
+
+function binWeeks(flat) {
+  let binned = {}
+  for(let expense of flat) {
+    const lsun = getWeek(expense['created_time'])
+
+    if(binned[lsun]) {
+      binned[lsun].push(expense)
+    }
+    else {
+      binned[lsun] = [expense]
+    }
+  }
+  console.log(binned)
+  return binned
+}
+
+async function runQuery(){
+    response = await getData(NOTION_DB, {})
+    return binWeeks(formatResults(response))
+}
+
+
+module.exports = {
+    runQuery
+}
