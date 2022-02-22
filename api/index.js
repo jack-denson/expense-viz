5161173d07f79fcc2934b15ed9f4aa6f502713ee
Jack Denson <jackdenson47@gmail.com>
2022-02-22 01:18:45 -0500

    Serving build for prod

--- api/index.js
+++ api/index.js
@@ -197,7 +197,12 @@ app.get('/viz/:visualization', authenticateToken, cors_policy, async (req, res) 
 
 
 
-
+if( process.env.NODE_ENV === 'production') {
+  app.use( express.static(__dirname + '/public/') );
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  });
+}
 
 
 
